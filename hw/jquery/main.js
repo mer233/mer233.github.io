622888803026b5a3035d69446cba597cbf0e1f0f
mer233 <mer233@users.noreply.github.com>
2020-04-15 11:38:48 -0500

    changing id
    id

--- hw/jquery/main.js
+++ hw/jquery/main.js
@@ -1,12 +1,16 @@
 $ (document).ready(function(){
 console.log ("page loaded");
 
-$ ("#title").click(function(){
-$("#title"). html ("Italy in the heat of the summer");
-});
-$("#venice").mouseover(function(){
-  //change background color to pale aqua
+$("#title").click(function(){
+$("#title").html("Italy in the heat of the summer");
 
-  $("body").css("background-color","#BFD7EA");
+});
+$("#test").mouseover(function(){
+  //change background color to tuscany
+  $("body").css("background-color","#BAA898");
+});
+$("#content").mouseleave(function(){
+  //fadeIn h2
+$("#content").hide();
 });
 });
