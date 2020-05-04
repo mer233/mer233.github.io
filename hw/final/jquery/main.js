c0e089ff4b0c47be88541ec248263aba9beb78cd
mer233 <mer233@users.noreply.github.com>
2020-05-04 12:49:34 -0500

    show and hide buttons
    p1-5

--- hw/final/jquery/main.js
+++ hw/final/jquery/main.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
 console.log ("page loaded");
 
-$("#test").mouseover(function(){
+$("#title").mouseover(function(){
+  //change text color to orange
   $("#title").css("color","#EF946C");
 });
+$("#title").mouseup(function(){
+$("#title").html("Mackenzie Ellen Roberts");
+});
+
+$("#button1").click(function(){
+  //when you click the button show the list
+  $(".blurb").show();
+});
+$("#button2").click(function(){
+  //when you click the button hide the list
+  $(".blurb").hide();
+});
+
 });
